9c195aeb3e70cfff285fde8c87a7abe0f7ff18d2
Cory Johns <johnsca@gmail.com>
2020-11-11 05:52:23 -0500

    Refactors based on discussion on Discourse (#2)
    See https://discourse.charmhub.io/t/kubernetes-service-interface-with-operator-framework/3782

--- k8s_service.py
+++ k8s_service.py
@@ -1,20 +1,28 @@
-from ops.framework import Object, StoredState, EventBase, EventSource, ObjectEvents
+from functools import cached_property  # nb: requires Python 3.8+
+
+from ops.framework import (
+    Object,
+    StoredState,
+    EventBase,
+    EventSource,
+    ObjectEvents,
+)
 from operator import attrgetter
 from hashlib import md5
-import logging
 
 
-class K8sServiceAvailable(EventBase):
+class K8sServicesChanged(EventBase):
     pass
 
 
 class RequireK8sServiceEvents(ObjectEvents):
-    k8s_service_available = EventSource(K8sServiceAvailable)
+    k8s_services_changed = EventSource(K8sServicesChanged)
 
 
 class ProvideK8sService(Object):
     def __init__(self, charm, relation_name, service_name, service_port):
         super().__init__(charm, relation_name)
+        # Ensure all related apps are sent the latest data, in case it changed.
         for relation in self.model.relations[relation_name]:
             relation.data[charm.app].update({
                 "service-name": service_name,
@@ -28,21 +36,42 @@ class RequireK8sService(Object):
 
     def __init__(self, charm, relation_name):
         super().__init__(charm, relation_name)
+        self._relation_name = relation_name
         self.state.set_default(data_hash=None)
+        self.framework.observe(charm.on[relation_name].relation_joined,
+                               self._check_services)
+        self.framework.observe(charm.on[relation_name].relation_changed,
+                               self._check_services)
 
-        self.services = []
-        for relation in sorted(self.model.relations[relation_name], key=attrgetter('id')):
+    @property
+    def _relations(self):
+        return self.model.relations[self._relation_name]
+
+    @cached_property
+    def services(self):
+        services = []
+        for relation in sorted(self._relations, key=attrgetter('id')):
             if not relation.app:
                 continue
             service_name = relation.data[relation.app].get("service-name")
             service_port = relation.data[relation.app].get("service-port")
             if service_name and service_port:
-                self.services.append((service_name, service_port))
+                services.append((service_name, service_port))
+        return services
 
-        self.is_available = bool(self.services)
-        self.is_created = bool(self.model.relations[relation_name])
+    @property
+    def is_available(self):
+        return len(self.services) > 0
 
-        data_hash = md5(str(self.services).encode('utf8')).hexdigest()
-        if self.is_available and self.state.data_hash != data_hash:
-            self.state.data_hash = data_hash
-            self.on.k8s_service_available.emit()
+    @property
+    def is_created(self):
+        return len(self._relations) > 0
+
+    @cached_property
+    def _data_hash(self):
+        return md5(str(self.services).encode('utf8')).hexdigest()
+
+    def _check_services(self, event):
+        if self.is_available and self.state.data_hash != self._data_hash:
+            self.state.data_hash = self._data_hash
+            self.on.k8s_services_changed.emit()
